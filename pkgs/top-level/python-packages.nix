ddc5946e6783452e2c382f673fb3b66add0256b8
Hugo Herter <git@hugoherter.com>
2025-04-17 12:52:37 +0200

    python312Packages.unsloth: init at 2025-02-v2

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -18359,6 +18359,8 @@ self: super: with self; {
 
   unrpa = callPackage ../development/python-modules/unrpa { };
 
+  unsloth = callPackage ../development/python-modules/unsloth { };
+
   unsloth-zoo = callPackage ../development/python-modules/unsloth-zoo { };
 
   unstructured = callPackage ../development/python-modules/unstructured { };
